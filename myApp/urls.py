e307841b3cb0b941e110c17d4673028f2dbd28f4
Jvictorio09 <juliavictorio16@gmail.com>
2025-08-25 00:51:40 +0800

    udpated

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path("contact/submit/", views.contact_submit, name="contact_submit"),
-    path("assistant/chat/", views.ai_chat, name="ai_chat"),
+    #path("contact/submit/", views.contact_submit, name="contact_submit"),
+    #path("assistant/chat/", views.ai_chat, name="ai_chat"),
 
     path("health/", lambda r: HttpResponse("ok", content_type="text/plain")),
 ]
